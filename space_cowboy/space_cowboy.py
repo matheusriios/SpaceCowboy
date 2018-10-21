16a4fccad2f8cf17c1f982e6ba2de43df3d0669f
Giovanni Armane <giovanniarmane@gmail.com>
2018-10-21 03:06:44 -0300

    GameObject prototype testing

--- space_cowboy/space_cowboy.py
+++ space_cowboy/space_cowboy.py
@@ -6,7 +6,12 @@ __all__ = (
 import os
 import pygame
 from pygame.locals import (
+    K_a,
+    K_d,
+    K_s,
+    K_w,
     K_DOWN,
+    K_ESCAPE,
     K_LEFT,
     K_RIGHT,
     K_UP,
@@ -27,8 +32,24 @@ from consts import (
 )
 from typedefs import (
     Colorkey,
-    Event
+    Event,
+    ScreenSize,
 )
+
+
+class GameObject:
+
+    def __init__(self, image, position, speed):
+
+        self.image = image
+        self.position = image.get_rect().move(*position)
+        self.speed = speed
+
+    def move(self, direction):
+
+        vec_x, vec_y = direction
+        self.position = self.position.move(vec_x * self.speed, vec_y * self.speed)
+
 
 
 def load_image(path: str) -> Surface:
@@ -60,15 +81,31 @@ def load_sound(path: str) -> Sound:
     return sound
 
 
-def main() -> None:
+def quit_pygame():
+
+    pygame.quit()
+
+
+def init_pygame():
 
     # TODO:  Negative values for Linux PulseAudio. Verify if Windows support negative values.
     pygame.mixer.pre_init(44100, -16, 2, 1024*4)
     pygame.init()
 
-    screen = pygame.display.set_mode((1024, 768), True, 32)
-    pygame.display.set_caption("Space Cowboy")
-    pygame.mouse.set_visible(False)
+
+def init_screen(caption: str, size: ScreenSize, depth: int, is_fullscreen: bool, is_mouse_visible: bool) -> Surface:
+
+    screen = pygame.display.set_mode(size, is_fullscreen, depth)
+    pygame.display.set_caption(caption)
+    pygame.mouse.set_visible(is_mouse_visible)
+    return screen
+
+
+def main() -> None:
+
+    init_pygame()
+
+    screen = init_screen("Space Cowboy", (1024, 768), 32, True, True)
 
     background = pygame.Surface(screen.get_size())
     background = background.convert()
@@ -80,42 +117,55 @@ def main() -> None:
     textpos.centerx = background.get_rect().centerx
     background.blit(text, textpos)
 
-    test_image1 = load_image_surface_alpha(os.path.join('tests', 'test1.png'))
-    test_image2 = load_image_surface_alpha(os.path.join('tests', 'test2.png'))
-    test_image2_rect = test_image2.get_rect()
+
+    ship_img = load_image_surface_alpha(os.path.join('tests', 'test1.png'))
+    asteroid_img = load_image_surface_alpha(os.path.join('tests', 'test2.png'))
+
+    ship = GameObject(ship_img, (100, 100), 10)
+    asteroid = GameObject(asteroid_img, (0, 0), 5)
+
+    objects = [ship, asteroid]
+
     test_sound_ogg = load_sound(os.path.join('tests', 'test1.ogg'))
     test_sound_wav = load_sound(os.path.join('tests', 'test1.wav'))
 
     screen.blit(background, (0, 0))
-    screen.blit(test_image2, (0, 10))
-    screen.blit(test_image1, (0, 0))
     pygame.display.flip()
 
     while True:
         event: Event
         for event in pygame.event.get():
             if event.type == QUIT:
-                pygame.quit()
-            elif event.type == MOUSEBUTTONDOWN:
+                return quit_pygame()
+            if event.type == KEYDOWN and event.key == K_ESCAPE:
+                return quit_pygame()
+            if event.type == MOUSEBUTTONDOWN:
                 button = event.button
                 if button == 1:
                     test_sound_ogg.play()
                 elif button == 3:
                     test_sound_wav.play()
             elif event.type == KEYDOWN:
-                key = event.key
-                if key == K_DOWN:
-                    test_image2_rect = test_image2_rect.move(0, 10)
-                elif key == K_LEFT:
-                    test_image2_rect = test_image2_rect.move(-10, 0)
-                elif key == K_RIGHT:
-                    test_image2_rect = test_image2_rect.move(10, 0)
-                elif key == K_UP:
-                    test_image2_rect = test_image2_rect.move(0, -10)
+                if event.key == K_a:
+                    asteroid.move((-1, 0))
+                elif event.key == K_d:
+                    asteroid.move((1, 0))
+                elif event.key == K_s:
+                    asteroid.move((0, 1))
+                elif event.key == K_w:
+                    asteroid.move((0, -1))
+                elif event.key == K_DOWN:
+                    ship.move((0, 1))
+                elif event.key == K_LEFT:
+                    ship.move((-1, 0))
+                elif event.key == K_RIGHT:
+                    ship.move((1, 0))
+                elif event.key == K_UP:
+                    ship.move((0, -1))
 
         screen.blit(background, (0, 0))
-        screen.blit(test_image2, test_image2_rect)
-        screen.blit(test_image1, pygame.mouse.get_pos())
+        for obj in objects:
+            screen.blit(obj.image, obj.position)
         pygame.display.flip()
 
 
