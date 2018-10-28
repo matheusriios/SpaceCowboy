3fb48b342e7ff847f3d9f2807ef1da4db10e5070
Giovanni Armane <giovanniarmane@gmail.com>
2018-10-28 18:03:20 -0300

    Define ship game object class

--- space_cowboy/core/game/objects.py
+++ space_cowboy/core/game/objects.py
@@ -1,4 +1,7 @@
-__all__ = ('Asteroid',)
+__all__ = (
+    'Asteroid',
+    'Ship',
+)
 
 
 from typing import Iterable
@@ -38,6 +41,43 @@ class Asteroid(Sprite):
 
 
     def update(self) -> None:
+
+        vec: Vector2 = self.direction * self.speed
+        self.rect.x += vec.x
+        self.rect.y += vec.y
+
+
+class Ship(Sprite):
+
+    def __init__(self, position: Iterable, direction: Iterable, speed: float, image: Surface):
+
+        super().__init__()
+        self.__direction = Vector2(direction)
+        self.speed = speed
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = position
+
+
+    @property
+    def direction(self) -> Vector2:
+
+        return self.__direction
+
+
+    @direction.setter
+    def direction(self, value: Iterable) -> None:
+
+        self.direction = Vector2(value).normalize_ip()
+
+
+    @property
+    def position(self) -> Vector2:
+
+        return Vector2(self.rect.x, self.rect.y)
+
+
+    def update(self):
 
         vec: Vector2 = self.direction * self.speed
         self.rect.x += vec.x
