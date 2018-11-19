e96864db30a6679072f4bbade652171520afcf6c
Giovanni Armane <giovanniarmane@gmail.com>
2018-11-18 23:52:57 -0200

    Modify comment for clarity

--- space_cowboy/core/handlers/initialization.py
+++ space_cowboy/core/handlers/initialization.py
@@ -19,7 +19,7 @@ def init_clock() -> Clock:
 def init_pygame():
 
     # TODO Negative values for Linux PulseAudio. Verify if Windows support negative values.
-    # TODO If there is another application using PulseAudio, the program can't start.  
+    # FIXME If there is another application using PulseAudio, the program can't start.  
     pygame.mixer.pre_init(44100, -16, 2, 1024*4)
     pygame.init()
 
