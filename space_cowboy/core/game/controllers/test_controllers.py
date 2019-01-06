1f6f9f0cb4bcfa689431f77093700ced274879d4
Giovanni Armane <giovanniarmane@gmail.com>
2019-01-06 03:08:32 -0200

    Remove unnecessary references

--- space_cowboy/core/game/controllers/test_controllers.py
+++ space_cowboy/core/game/controllers/test_controllers.py
@@ -20,19 +20,17 @@ class GameTestController001:
 
         self.__display = display
         self.__event_system = event_system
-        self.__images_loader = images_loader
 
-        self.__background = self.__images_loader.smoothscale(
-            self.__images_loader.load_surface(
+        self.__background = images_loader.smoothscale(
+            images_loader.load_surface(
                 os.path.join('backgrounds', 'stars_blue.png')),
             self.__display.get_size())
         self.__display.blit(self.__background, (0, 0))
 
-        self.__player = build_player_ship(
-            self.__display.get_rect().center, (0, 0), 10, self.__event_system, self.__images_loader)
-
-        self.__player_group = GroupSingle(self.__player)
-        self.__sprites_group = Group(self.__player)
+        player = build_player_ship(
+            self.__display.get_rect().center, (0, 0), 10, self.__event_system, images_loader)
+        self.__player_group = GroupSingle(player)
+        self.__sprites_group = Group(player)
 
     def process_events(self):
 
